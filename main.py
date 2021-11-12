bda8bcee63cd69e364f3d1b0ea5d5e0937c81cd7
boccci <93566931+boccci@users.noreply.github.com>
2021-11-11 18:05:36 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math as math
 import pandas as pd
-from decimal import *
+import sys
+
 
 data = pd.read_csv('data_dup.dat', sep='/=', header = None, skipinitialspace=False, names =['value','name'])
 df = data.copy()
@@ -64,6 +65,12 @@ B12 = np.array([40,45, 55.0, 1, 111, 50, 58.0, -1, 1372.00], dtype = float)
 
 #functional definitions
 
+def sat_locs(s_i, t):
+    u = np.array([s_i[0], s_i[1], s_i[2]])
+    v = np.array([s_i[3], s_i[4], s_i[5]])
+    x = (R + s_i[7]) * (-1 * u * np.sin(2 * pi * t / p + s_i[8]) + v * np.cos(2 * pi * t / p + s_i[8]))
+    return x
+
 def deg2rad(deg): #this nested form is the only way to get the correct number, s/(360*60^2) yields a wildly different answer (+/-.0004)
 
     [d,m,s,sign] = [deg[0],deg[1],deg[2],deg[3]]
@@ -81,12 +88,12 @@ def rad2cart(rad):
     cart = [x,y,z]
     return cart
 
-def deg2cart(deg): #if you want to do it all at once.
+def deg2cart(deg):  #if you want to do it all at once.
 
-    [d, m, s, sign] = [deg[0], deg[1], deg[2], deg[3]]
-    Lat_d2r = 2 * pi * sign * (d + (m + s / 60) / 60) / 360
-    [d, m, s, sign] = [deg[4], deg[5], deg[6], deg[7]]
-    Long_d2r = 2 * pi * sign * (d + (m + s / 60) / 60) / 360
+    [dt, mt, st, sign] = [deg[0], deg[1], deg[2], deg[3]]
+    Lat_d2r = 2 * pi * sign * (dt + (mt + st / 60) / 60) / 360
+    [dg, mg, sg, sign] = [deg[4], deg[5], deg[6], deg[7]]
+    Long_d2r = 2 * pi * sign * (dg + (mg + sg / 60) / 60) / 360
     d2r = [Lat_d2r, Long_d2r, deg[8]]
     x = (R + d2r[2]) * np.cos(d2r[0]) * np.cos(d2r[1])
     y = (R + d2r[2]) * np.cos(d2r[0]) * np.sin(d2r[1])
@@ -106,16 +113,16 @@ def rad2deg(rad):
     degs = np.array([[1,2,3],[1,2,3]], dtype=float) #spent way too long trying to figure out why i kept getting a integer...always declare your variable type robert.
     r = [0,1]
     signs = [0,0]
-    if rad[0] < 0: #theres a way to make this look way better didnt want to work with nested loops
+    if rad[0] < 0: #theres a way to make this look way better didn't want to work with nested loops
         signs[0] = -1
     else:
-        signs[0] =  1
+        signs[0] = 1
     if rad[1] < 0:
         signs[1] = -1
     else:
-        signs[1] =  1
+        signs[1] = 1
     for n in r:
-        init = rad[n] * ((360 / (2)) / pi) * signs[n]
+        init = rad[n] * ((360 / 2) / pi) * signs[n]
         split = math.modf(init)
         degs[n,0] = split[1]
         inter= math.modf(split[0]*60)
@@ -123,7 +130,7 @@ def rad2deg(rad):
         inter2 = inter[0]*60
         degs[n,2] = inter2
 
-    deg = [degs[0,0], degs[0,1], degs[0,2], signs[0], degs[1,0], degs[1,1],float(degs[1,2]) , signs[1], rad[2]]
+    deg = [degs[0,0], degs[0,1], degs[0,2], signs[0], degs[1,0], degs[1,1],float(degs[1,2]), signs[1], rad[2]]
     return deg
 
 def rotation_offset(x,t_r): #this will compute x_v at t_v
@@ -134,20 +141,21 @@ def rotation_offset(x,t_r): #this will compute x_v at t_v
     x_off = np.dot(offset,x)
     return x_off
 
-def horiz_check(x_i, s_i_list, t_s): #cartesian coords, we can run this function recursively for sets of satellites and singular x with some for loops
+def horiz_check(x_i, s_i_list, t_s):  #cartesian coords, we can run this function recursively for sets of satellites and singular x with some for loops
     truth_list = []
     n = 0
     while n < 23:
         s_i = s_i_list[n]
-        u = np.array([s_i[0], s_i[1], s_i[2]])
-        v = np.array([s_i[3], s_i[4], s_i[5]])
-        s = (R+s_i[7])*(-1*u*np.sin(2*pi*t_s/p + s_i[8]) + v*np.cos(2*pi*t_s/p + s_i[8]))
+        # u = np.array([s_i[0], s_i[1], s_i[2]])
+        # v = np.array([s_i[3], s_i[4], s_i[5]])
+        # s = (R+s_i[7])*(-1*u*np.sin(2*pi*t_s/p + s_i[8]) + v*np.cos(2*pi*t_s/p + s_i[8]))
+        s = sat_locs(s_i,t_s)
         f = np.dot(x_i,s)
         g = np.dot(x_i,x_i)
         if f>g:
-            s_up = 1 #true
+            s_up = 1  #true
         else:
-            s_up = 0 #false
+            s_up = 0  #false
         truth_list.append(s_up)
         n = n+1
     return truth_list #boolean
@@ -158,21 +166,22 @@ def sat_time(x_v_i,t_v, s_i_list):  #first attempt at a newtons method code - al
     n = 0
     t_0 = t_v
     t = [t_0, t_v]
-    while n <23:
+    while n < 23:
         s_i = s_i_list[n]
         u = np.array([s_i[0],s_i[1],s_i[2]])
         v = np.array([s_i[3],s_i[4],s_i[5]])
         h = s_i[7]
         phase = s_i[8]
 
-        sat_0 = (R+h)*(-1*u*np.sin(2*pi*t[0]/p + phase) + v*np.cos(2*pi*t[0]/p + phase))   #x_s at t_v
+        # sat_0 = (R+h)*(-1*u*np.sin(2*pi*t[0]/p + phase) + v*np.cos(2*pi*t[0]/p + phase))   #x_s at t_v
+        sat_0 = sat_locs(s_i, t_0)
         dif_0 = (sat_0 - x_v_i)
 
         t[0] = t_v - np.linalg.norm(dif_0)/c
         errmax = .01/c
 
         while errtime >= errmax:
-            sat = (R + h)*(-1*u*np.sin(2*pi*t[0]/p + phase) + v*np.cos(2*pi*t[0]/p + phase ))
+            sat = sat_locs(s_i, t[0])
             dif = (sat - x_v_i)
             f = np.linalg.norm(dif)**2-c**2*(t_v - t[0])**2
             fpr = (4*pi*(R + h))*np.dot(dif,sat) + 2*(c**2)*(t_v - t[0])
@@ -195,6 +204,8 @@ def above_index(list,item):
             locs.append(loc)
             start_at = loc
     return locs
+
+#have a better version of this above
 
 # def sat_time(x_v_i,t_v, u_s,v_s,phase_s):  #first attempt at a newtons method code
 #     errtime = 0  #a lot of ugly declarations
@@ -224,8 +235,8 @@ def above_index(list,item):
 
 
 #testing
-x_v_c =  deg2cart(B12) #B12 here will be replaced by a read-in x_v from vehicle.log
-t_v = 12123.0 #will be read in
+x_v_c = deg2cart(B12)  #B12 here will be replaced by a read-in x_v from vehicle.log
+t_v = 12123.0  #will be read in
 x_v_t = rotation_offset(x_v_c, t_v)
 
 n = 0
@@ -234,24 +245,119 @@ t_s = sat_time(x_v_t,t_v,sats)
 s_ab = horiz_check(x_v_t,sats,t_v)
 above = above_index(s_ab,1)
 
-
 def writeout(t_s_list, above_list):
     sat_exp = []
+    # sat_total = np.array([],[],[])
     n_w = 0
-    len_w = len(above_list)-1
-    while n_w <= len_w:
-        sat_exp.append([above_list[n_w], t_s_list[above_list[n_w]]])
+    len_w = len(above_list)
+    print(len_w)
+    print(above_list)
+    while n_w < len_w:
+        s_x = sat_locs(sats[above_list[n_w]],t_s_list[above_list[n_w]])
+        x = s_x[0]
+        y = s_x[1]
+        z = s_x[2]
+        sat_exp = [above_list[n_w], t_s_list[above_list[n_w]], x, y, z]
+        sys.stdout.write("{} {} {} {} {}".format(sat_exp[0],sat_exp[1],sat_exp[2],sat_exp[3],sat_exp[4]))
+        # for i in range(0,len(sat_exp)):
+        #     print(sat_exp[i], end=" "),
+        # sat_total.append(sat_exp)
         n_w = n_w + 1
+        print()
+    if n_w == len_w:
+        x = 0
     return sat_exp
 
 xp = writeout(t_s,above)
 
-print(t_s)
-print(s_ab)
-print(above)
-print(*xp)
+indeces = xp #read in the standard output - a list of satellites in satndard form as listed above where s_i is a throuple, we'll have to store a variable for that
+                #                                                                                                  throuple and assign a np.array to it
 
-#this is where reciever code will be for now - we will feed out (i, t_i, s_i)
+def Jacobian(s_i_list, t_i_list, x_k):
+    J = np.array([],[],[])
+    k = 0
+    j = 0
+    while j <= 2:
+        while k <= 2:
+            s_i = sat_locs(s_i_list[j], t_i_list[j])
+            s_i_1 = sat_locs(s_i_list[j+1], t_i_list[j+1])
+            J[j, k] = (s_i[k]-x_k[k])/np.linalg.norm(s_i[k]-x_k[k])-(s_i_1[k]-x_k[k])/np.linalg.norm(s_i_1[k]-x_k[k])
+            k = k + 1
+        j = j + 1
+    return J
+
+def Func(s_i_list, t_i_list, x_k):
+    F = np.array([],[],[])
+    j = 0
+    k = 0
+    while j <= 2:
+        while k <= 2:
+            s_i = sat_locs(s_i_list[j], t_i_list[j])
+            s_i_1 = sat_locs(s_i_list[j + 1], t_i_list[j + 1])
+            F[j,k] = np.linalg.norm(s_i_1[k]-x_k[k]) - np.linalg.norm(s_i[k]-x_k[k]) - c*(t_i_list[j]-t_i_list[j+1])
+            k = k + 1
+        j = j + 1
+    return F
+
+def LU(A, b):
+    N = len(A)
+    L = np.zeros(shape=(N,N))
+    U = np.zeros(shape=(N,N))
+    x = np.zeros(shape=(N,1))
+    y = np.zeros(shape=(N,1))
+    for i in range(N):
+
+        for j in range(i, N): #
+            sum = 0
+            for k in range(i):
+                sum = sum + L[i,k]*U[k,j]
+            U[i,j] = A[i,j] - sum
+        for j in range(i,N):
+            if i == j:
+                L[i,i] = 1
+            else:
+                sum = 0
+                for k in range(i):
+                    sum = sum + L[j,k]*U[k,i]
+                L[j,i] = (A[j,i]-sum)/U[i,i]
+    while i <= N:
+        if i == 0:
+            y[0,0] = b[0,0]
+            i = i + 1
+        else:
+            sum = 0
+            for k in range(i):
+                sum = sum + y[i-1,0]*L[i,i-0]
+                y[i,0] = b[i,0] - sum
+            i = i + 1
+    while i >= 0:
+        if i == N:
+            x[N,0] = y[N,0]/U[N,N]
+            i = i - 1
+        else:
+            sum = 0
+            for k in range(i+1,N):
+                sum = sum + U[i,k]*x[k,0]
+            x[i,0] = (y[i,0]- sum)/U[i,i]
+    return x
+
+def Newt(s_i_list,t_i_list,x_0):
+    x_k = x_0
+    s = np.zeros(shape = (3,1))
+    while np.linalg.norm(s) >= .0001:
+        J = Jacobian(s_i_list, t_i_list, x_k)
+        F = Func(s_i_list, t_i_list, x_k)
+        s = LU(J,-F)
+        x_k[0] = x_k[0] + s
+    return x_k
+
+x_b12 = Newt(above,t_s, deg2cart(B12))
+x_b12rad = cart2rad(x_b12)
+x_b12deg = rad2deg(x_b12rad)
+
+print(*x_b12deg)
+
+
 
 
 
